d62cc5af789b357c4e83984935c8024cb505fe18
Scott Walls <scottwalls2008@gmail.com>
2022-09-30 15:01:32 -0500

    More nvim config stuff

--- config/nvim/lua/numine/lir.lua
+++ config/nvim/lua/numine/lir.lua
@@ -26,7 +26,7 @@ require("lir").setup({
     show_hidden_files = true,
     devicons_enable = true,
     mappings = {
-        ["<CR>"] = actions.edit,
+        ["l"] = actions.edit,
         ["<C-s>"] = actions.split,
         ["<C-v>"] = actions.vsplit,
         ["<C-t>"] = actions.tabedit,
@@ -34,8 +34,8 @@ require("lir").setup({
         ["h"] = actions.up,
         ["q"] = actions.quit,
 
-        ["K"] = actions.mkdir,
-        ["N"] = actions.newfile,
+        ["d"] = actions.mkdir,
+        ["%"] = actions.newfile,
         ["R"] = actions.rename,
         ["@"] = actions.cd,
         ["Y"] = actions.yank_path,
@@ -50,43 +50,43 @@ require("lir").setup({
         ["X"] = clipboard_actions.cut,
         ["P"] = clipboard_actions.paste,
     },
-    -- float = {
-    --     winblend = 0,
-    --     curdir_window = {
-    --         enable = false,
-    --         highlight_dirname = false,
-    --     },
+    float = {
+        winblend = 0,
+        curdir_window = {
+            enable = false,
+            highlight_dirname = false,
+        },
 
-    --     -- -- You can define a function that returns a table to be passed as the third
-    --     -- -- argument of nvim_open_win().
-    --     win_opts = function()
-    --       local width = math.floor(vim.o.columns * 0.8)
-    --       local height = math.floor(vim.o.lines * 0.8)
-    --       return {
-    --         border = {
-    --           "+", "─", "+", "│", "+", "─", "+", "│",
-    --         },
-    --         width = width,
-    --         height = height,
-    --         row = 1,
-    --         col = math.floor((vim.o.columns - width) / 2),
-    --       }
-    --     end,
-    -- },
+        -- -- You can define a function that returns a table to be passed as the third
+        -- -- argument of nvim_open_win().
+        win_opts = function()
+          local width = math.floor(vim.o.columns * 0.8)
+          local height = math.floor(vim.o.lines * 0.8)
+          return {
+            border = {
+              "+", "─", "+", "│", "+", "─", "+", "│",
+            },
+            width = width,
+            height = height,
+            row = 1,
+            col = math.floor((vim.o.columns - width) / 2),
+          }
+        end,
+    },
     -- hide_cursor = true,
-    -- on_init = function()
-    --     -- use visual mode
-    --     vim.api.nvim_buf_set_keymap(
-    --         0,
-    --         "x",
-    --         "J",
-    --         ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
-    --         { noremap = true, silent = true }
-    --     )
+    on_init = function()
+        -- use visual mode
+        vim.api.nvim_buf_set_keymap(
+            0,
+            "x",
+            "J",
+            ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+            { noremap = true, silent = true }
+        )
 
-    --     -- echo cwd
-    --     vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
-    -- end,
+        -- echo cwd
+        vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
+    end,
 })
 
 -- custom folder icon

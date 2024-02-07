257e478f1038dc3722d24d5e4a97b040393a97de
Scott Walls <scott.walls@intusurg.com>
2024-02-06 18:40:26 -0600

    Neovim updates

--- config/nvim/lua/numine/lazy/colors.lua
+++ config/nvim/lua/numine/lazy/colors.lua
@@ -1,6 +1,6 @@
 function ColorMyPencils(color)
     vim.opt.background = "dark"
-    color = color or "oxocarbon"
+    color = color or "monokai-pro"
     vim.cmd.colorscheme(color)
 
     local hl = function(thing, opts)
@@ -54,16 +54,71 @@ function ColorMyPencils(color)
     --     fg = "#5eacd3",
     -- })
 
-    -- hl("netrwDir", {
-    --     fg = "#5eacd3",
-    -- })
+    hl("netrwDir", {
+        fg = "#5eacd3",
+        bg = "none",
+    })
 
     hl("WinSeparator", { background = nil })
 end
 
 return {
-	"nyoom-engineering/oxocarbon.nvim",
-    config = function()
-        ColorMyPencils()
-    end,
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        config = function()
+            ColorMyPencils()
+        end,
+    },
+
+    {
+        "loctvl842/monokai-pro.nvim",
+        config = function()
+            require("monokai-pro").setup({
+                transparent_background = true,
+                terminal_colors = true,
+                devicons = true, -- highlight the icons of `nvim-web-devicons`
+                styles = {
+                    comment = { italic = true },
+                    keyword = { italic = false }, -- any other keyword
+                    type = { italic = false }, -- (preferred) int, long, char, etc
+                    storageclass = { italic = false }, -- static, register, volatile, etc
+                    structure = { italic = false }, -- struct, union, enum, etc
+                    parameter = { italic = false }, -- parameter pass in function
+                    annotation = { italic = false },
+                    tag_attribute = { italic = false }, -- attribute of tag in reactjs
+                },
+                filter = "classic",        -- classic | octagon | pro | machine | ristretto | spectrum
+                -- Enable this will disable filter option
+                day_night = {
+                    enable = false, -- turn off by default
+                    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+                    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+                },
+                inc_search = "background", -- underline | background
+                background_clear = {
+                    -- "float_win",
+                    -- "toggleterm",
+                    "telescope",
+                    -- "which-key",
+                    "renamer",
+                    -- "notify",
+                    -- "nvim-tree",
+                    -- "neo-tree",
+                    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+                }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+                plugins = {
+                    bufferline = {
+                        underline_selected = false,
+                        underline_visible = false,
+                    },
+                    indent_blankline = {
+                        context_highlight = "default", -- default | pro
+                        context_start_underline = false,
+                    },
+                },
+                ---@param c Colorscheme
+                override = function(c) end,
+            })
+        end,
+    }
 }
--- config/nvim/lua/numine/set.lua
+++ config/nvim/lua/numine/set.lua
@@ -28,6 +28,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.opt.laststatus = 3
